1595a1de1a6db758d6291f8d0dcd7f4904e66c0e
Kyle Coffey <me@phytolizer.dev>
2022-03-11 03:15:08 -0600

    window

--- source/hello.c
+++ source/hello.c
@@ -1,5 +1,26 @@
+#include <GLFW/glfw3.h>
+#include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <vulkan/vulkan_core.h>
 
 int main(void) {
-    puts("hello world");
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window =
+        glfwCreateWindow(800, 600, "Vulkan window", NULL, NULL);
+
+    uint32_t extension_count = 0;
+    vkEnumerateInstanceExtensionProperties(NULL, &extension_count, NULL);
+
+    printf("%" PRIu32 " extensions supported\n", extension_count);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
