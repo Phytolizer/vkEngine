4564f55ec3aeae315ef9e895902b8bb65fc6aa10
Kyle Coffey <me@phytolizer.dev>
2022-04-13 01:39:24 -0500

    formatting

--- source/main.c
+++ source/main.c
@@ -1,20 +1,19 @@
+#include "lib.h"
+
 #include <stddef.h>
 #include <stdio.h>
 
-#include "lib.h"
+int main(int argc, const char* argv[]) {
+	struct library lib = create_library();
 
-int main(int argc, const char* argv[])
-{
-  struct library lib = create_library();
+	(void)argc;
+	(void)argv;
 
-  (void)argc;
-  (void)argv;
-
-  if (lib.name == NULL) {
-    (void)puts("Hello from unknown! (JSON parsing failed in library)");
-  } else {
-    (void)printf("Hello from %s!", lib.name);
-  }
-  destroy_library(&lib);
-  return 0;
+	if (lib.name == NULL) {
+		(void)puts("Hello from unknown! (JSON parsing failed in library)");
+	} else {
+		(void)printf("Hello from %s!", lib.name);
+	}
+	destroy_library(&lib);
+	return 0;
 }
